0d273467a09adb4d133102e5ef40a14d8e87b487
HENOK GETAHUN <162030691+henok-getahun@users.noreply.github.com>
2024-10-05 01:03:49 +0300

    Update Solution_3.py(ListComprehensions).py

--- Solution_3.py(ListComprehensions).py
+++ Solution_3.py(ListComprehensions).py
@@ -4,9 +4,7 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
     
-    
     result = []
-    
     for i in range(x + 1):
         for j in range(y + 1):
             for k in range(z + 1):
