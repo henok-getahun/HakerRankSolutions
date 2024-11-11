1188c25ef27ce690e0349c357729c09f04a42685
HENOK GETAHUN <162030691+henok-getahun@users.noreply.github.com>
2024-11-11 16:34:45 +0300

    Create Solution_5(Counting sort 1).py

---
+++ Solution_5(Counting sort 1).py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    maxElement = max(arr)
+    minElement = min(arr) 
+    INTEGER_ARRAY = [0]*(maxElement + 1) 
+    for i in range(len(arr)):
+        curr= arr[i]
+        INTEGER_ARRAY[curr] += 1
+    
+    return INTEGER_ARRAY
+              
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
